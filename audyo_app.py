ed534fd3d7b06b1ba285a0192a81954284d62aa5
Marcus Gee <72676962+mgee-4@users.noreply.github.com>
2023-11-08 22:34:36 -0700

    Updated display functionality
    The chords properly display now

--- audyo_app.py
+++ audyo_app.py
@@ -13,8 +13,10 @@ class Audyo(App):
     file_path = StringProperty('Choose File')
     sound = ObjectProperty()
     cchord = StringProperty('None')
-    clist = ObjectProperty()
     idx = ObjectProperty(0)
+    clist = ObjectProperty()
+    is_playing = ObjectProperty(False)
+    sound_pos = ObjectProperty(0)
 
     def build(self):
         return Builder.load_file('audyo_app.kv')
@@ -30,32 +32,40 @@ class Audyo(App):
         self.sound = SoundLoader.load(self.file_path)
         chords = chordino.extract(self.file_path)
         self.clist = chords
+        self.cchord = self.clist[0][0]
+
+    def toggle_play(self):
+        if not self.clist:
+            return
+        if self.is_playing:
+            self.pause()
+        else:
+            self.play()
 
     def play(self):
         if self.clist:
-            self.sound_pos = 0
-            self.sound.play()
+            if self.sound_pos == 0:
+                self.sound.play()
+            else:
+                self.sound.seek(self.sound_pos)
+                self.sound.play()
+            self.is_playing = True
+            Clock.schedule_interval(self.update_chord, 0.1)
 
     def pause(self):
         if self.clist:
             self.sound_pos = self.sound.get_pos()
             self.sound.stop()
+            self.is_playing = False
+            Clock.unschedule(self.update_chord)
 
-    def resume(self):
-        if self.clist:
-            self.sound.play()
-            Clock.schedule_once(self.do_seek)
-
-    def do_seek(self, dt):
-        self.sound.seek(self.sound_pos)
+    def update_chord(self, dt):
+        if self.idx < len(self.clist) and self.sound.get_pos() >= self.clist[self.idx][1]:
+            self.cchord = self.clist[self.idx][0]
+            self.idx += 1
 
     def chords(self):
         print(self.clist)
-
-    def nc(self):
-        if self.idx < len(self.clist):
-            self.cchord = self.clist[self.idx][0]
-            self.idx += 1
 
 if __name__ == "__main__":
     Audyo().run()
